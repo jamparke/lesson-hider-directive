1e25880bda6644c5bf363da3913c5a636004566a
Andy Parker <andyparker84@gmail.com>
2017-07-25 16:35:38 -0500

    Project complete

---
+++ lessonCtrl.js
@@ -0,0 +1,10 @@
+angular.module("directivePractice").controller("lessonCtrl", function($scope, lessonService) {
+    $scope.lessons = ['Services', 'Routing', 'Directives', 'Review', 'Firebase', 'No server project', 'Node', 'Express', 'Mongo'];
+    $scope.announceDay = function(lesson, day){
+        if (day) {
+            alert(lesson + ' is active on ' + day + ".");
+        } else {
+            alert(lesson + ' is not on the schedule.');
+        }
+    };
+});
